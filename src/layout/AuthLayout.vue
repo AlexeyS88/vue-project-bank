6eff33a82afba53ca8a548993772e172489cb00c
AlexeyS88 <antey343@gmail.com>
2025-02-17 21:39:18 +0300

    Удалить .vscode из репозитория

--- src/layout/AuthLayout.vue
+++ src/layout/AuthLayout.vue
@@ -1,14 +1,14 @@
 <template>
-  <the-navbar></the-navbar>
   <div class="container with-nav">
+    <app-message></app-message>
     <router-view />
   </div>
 </template>
 
 <script>
-import TheNavbar from "@/components/TheNavbar.vue";
+import AppMessage from "@/components/ui/AppMessage.vue";
 export default {
-  components: {TheNavbar}
+  components: {AppMessage}
 }
 </script>
 
