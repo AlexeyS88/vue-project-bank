99cf0c9dfa98854df998a9eaa6be4e72af64a00c
AlexeyS88 <antey343@gmail.com>
2025-02-18 20:49:26 +0300

    add page

---
+++ src/components/request/RequestModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <form>
+    <div class="form-control">
+      <label for="name">ФИО</label>
+      <input type="text" name="name">
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
